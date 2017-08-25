f7f0fa330fb5ac01de0daa1156dc6f4d4ff0a05a
31kish <keishi.k@meltaba.com>
2017-08-25 18:54:30 +0900

    モデルを修正
    バリデーションを追加

--- app/models/user.go
+++ app/models/user.go
@@ -2,26 +2,41 @@ package models
 
 import (
 	"time"
+
+	"github.com/revel/revel"
 )
 
-// User --
+// User -
 type User struct {
-	ID         uint64 `gorm:"primary_key"`
-	MailAdress string `sql:"size:255" validate:"email"`
-	Name       string `sql:"size255" validate:"min=1"`
-	Password   string `sql:"size255"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	DeletedAt  *time.Time
+	ID             uint64 `gorm:"primary_key"`
+	MailAdress     string `sql:"size:255"`
+	Name           string `sql:"size:255"`
+	Password       string `sql:"-"`
+	HashedPassword []byte
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      *time.Time
 }
 
-// UserAdmin --
+// UserAdmin -
 type UserAdmin struct {
-	ID         uint64 `gorm:"primary_key"`
-	MailAdress string `sql:"size:255" validate:"email"`
-	Name       string `sql:"size255" validate:"min=1"`
-	Password   string `sql:"size255"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	DeletedAt  *time.Time
+	ID             uint64 `gorm:"primary_key"`
+	MailAdress     string `sql:"size:255"`
+	Name           string `sql:"size:255"`
+	Password       string `sql:"-"`
+	HashedPassword []byte
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      *time.Time
+}
+
+// Validate -
+func (userAdmin *UserAdmin) Validate(v *revel.Validation) {
+	v.Check(
+		userAdmin.Name,
+		revel.MaxSize{Max: 15},
+		revel.MinSize{Min: 3},
+	).Message("3文字以上、15文字以内で入力してください")
+
+	v.Email(userAdmin.MailAdress).Message("メールアドレスの形式で入力してください")
 }
