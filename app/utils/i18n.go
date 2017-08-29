c930c66f69724bdb6f2fc61c16c86c5347b02204
31kish <keishi.k@meltaba.com>
2017-08-29 14:51:19 +0900

    メッセージ一元化

--- app/utils/i18n.go
+++ app/utils/i18n.go
@@ -3,22 +3,42 @@ package utils
 import (
 	"io/ioutil"
 	"log"
+	"reflect"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
-// I18n -
-var I18n interface {
-	Translate(string) string
+type util_i18n interface {
+	Translate(key string) string
 }
 
-type i18n struct {
+var I18n util_i18n = i18n{}
+
+type i18n struct{}
+
+func (i18n) Translate(key string) string {
+	messages := loadMessageFile()
+	keys := strings.Split(key, ".")
+
+	for i := 0; i < len(keys)-1; i++ {
+		if val, ok := messages[keys[i]]; !ok || reflect.TypeOf(messages[keys[i]]).Kind() != reflect.Map {
+			log.Panicf("translation missing key: %#v val: %#v", keys[i], val)
+		}
+		messages = messages[keys[i]].(map[interface{}]interface{})
+	}
+
+	if val, ok := messages[keys[len(keys)-1]]; !ok {
+		log.Panicf("translation missing key: %#v val: %#v", keys[len(keys)-1], val)
+	}
+
+	return messages[keys[len(keys)-1]].(string)
 }
 
-func (i i18n) Translate(key string) string {
+func loadMessageFile() map[interface{}]interface{} {
 	buf, err := ioutil.ReadFile("messages/ja.yaml")
 	if err != nil {
-		return "translation missing"
+		panic(err)
 	}
 
 	m := make(map[interface{}]interface{})
@@ -27,6 +47,5 @@ func (i i18n) Translate(key string) string {
 		panic(err)
 	}
 
-	log.Printf("%s", m["a"])
-	return ""
+	return m
 }
