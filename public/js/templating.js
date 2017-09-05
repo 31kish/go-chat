50672a5d5ebd02ddc5a7dbfce51f9190cbc49dc8
31kish <keishi.k@meltaba.com>
2017-09-05 12:05:17 +0900

    レガシーなjavascriptを修正

--- public/js/templating.js
+++ public/js/templating.js
@@ -1,23 +1,23 @@
 // Simple JavaScript Templating
 // John Resig - https://johnresig.com/ - MIT Licensed
 (function(){
-  var cache = {};
-   
+  let cache = {};
+
   this.tmpl = function tmpl(str, data){
     // Figure out if we're getting a template, or if we need to
     // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
+    let fn = !/\W/.test(str) ?
       cache[str] = cache[str] ||
         tmpl(document.getElementById(str).innerHTML) :
-       
+
       // Generate a reusable function that will serve as a template
       // generator (and which will be cached).
       new Function("obj",
         "var p=[],print=function(){p.push.apply(p,arguments);};" +
-         
+
         // Introduce the data as local variables using with(){}
         "with(obj){p.push('" +
-         
+
         // Convert the template into pure JavaScript
         str
           .replace(/[\r\t\n]/g, " ")
