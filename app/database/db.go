89c9aee183355f111bf74b235b28d6fb07c69bef
31kish <keishi.k@meltaba.com>
2017-09-12 11:03:37 +0900

    Change db driver sqlite to mysql

--- app/database/db.go
+++ app/database/db.go
@@ -1,14 +1,19 @@
 package database
 
 import (
+	"fmt"
 	"go-chat/app/models"
 	"log"
 
 	"github.com/revel/revel"
 
 	"github.com/jinzhu/gorm"
+
 	// sqlite -
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	// mysql -
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Connection -
@@ -16,7 +21,9 @@ var Connection **gorm.DB
 
 // Init -
 func Init() {
-	db, err := gorm.Open("sqlite3", "./go-chat.db")
+	// db, err := gorm.Open("sqlite3", "./go-chat.db")
+	connectionOption := getConnectionOption()
+	db, err := gorm.Open("mysql", connectionOption)
 
 	if err != nil {
 		log.Panicf("faild to connect database %#v", err)
@@ -30,4 +37,28 @@ func Init() {
 	Connection = &db
 
 	log.Println("connected to database")
+}
+
+func getConnectionOption() string {
+	user, found := revel.Config.String("db.user")
+	if !found {
+		panic("db user is required")
+	}
+
+	pass, found := revel.Config.String("db.password")
+	if !found {
+		panic("db password is required")
+	}
+
+	name, found := revel.Config.String("db.name")
+	if !found {
+		panic("db name is required")
+	}
+
+	host := revel.Config.StringDefault("db.host", "localhost")
+	port := revel.Config.StringDefault("db.port", "3306")
+	protocol := revel.Config.StringDefault("db.protocol", "tcp")
+
+	revel.ERROR.Printf("%s:%s@%s([%s]:%s)/%s", user, pass, protocol, host, port, name)
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s", user, pass, protocol, host, port, name)
 }
